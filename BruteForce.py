2935275c8daadab40aaf0f04440fc8a44d72d7e3
Mohammed sithik <108626817+mohdsithik@users.noreply.github.com>
2022-08-23 21:15:13 +0530

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,12 @@
+from itertools import combinations_with_replacement as rep
+amount = 13
+coins = [1,2,5]
+arr = []
+for i in range(1,amount+1):
+    arr.append(rep(coins,i))
+temp=[]
+for j in arr:
+    for k in j:
+        if sum(k)==amount:
+            temp.append(k)
+print(temp[0])        
