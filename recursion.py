ffbc1d7b80c4d860c14a926a7758f9cf48324fd6
Mohammed sithik <108626817+mohdsithik@users.noreply.github.com>
2022-08-25 14:02:44 +0530

    recursion

--- recursion.py
+++ recursion.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-a=[1,2,5,10,20,50,100,200,500,2000]
-amount=int(input("Enter the amount : "))
-=======
-a=[1,2,5]
-# amount=int(input("Enter the amount : "))
-amount=13
->>>>>>> bd50ad387914dd34eab0664da5c5856926861b25
-arr=[]
-
 def rec(a,amount,arr):
     if amount==0:
         return arr
